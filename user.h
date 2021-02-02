1649b5be0f12a41fb3aef6f86b2472ec29a33464
Nicki-Di <78273681+Nicki-Di@users.noreply.github.com>
2021-02-03 00:28:19 +0330

    new system calls added

--- user.h
+++ user.h
@@ -23,6 +23,9 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int getParentID(void);
+int getChildren(void);
+int getSyscallCounter(int);
 
 // ulib.c
 int stat(const char*, struct stat*);
