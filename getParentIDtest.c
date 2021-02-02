87cec4c332da1f13ba7dd8b0c2eb203990c8f07b
Nicki-Di <78273681+Nicki-Di@users.noreply.github.com>
2021-02-03 00:32:58 +0330

    Create getParentIDtest.c

---
+++ getParentIDtest.c
@@ -0,0 +1,23 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(){
+
+     if (fork()== 0){
+        printf(1, "This is process %d and the parent id is %d\n", getpid(), getParentID());
+        }
+    else
+    { 
+        wait();
+    
+        if(fork() == 0){
+            printf(1, "This is process %d and the parent id is %d\n", getpid(), getParentID());
+        }
+        else{
+                    wait();
+
+        }
+    }
+    exit();
+}
