0ff7172082342be1f6907023a5e275f3b733d3c1
arta asadi <52131111+artaasadi@users.noreply.github.com>
2021-02-04 18:47:08 +0330

    Update defs.h
    adding tcounter and tcounterlock

--- defs.h
+++ defs.h
@@ -167,6 +167,8 @@ void            idtinit(void);
 extern uint     ticks;
 void            tvinit(void);
 extern struct spinlock tickslock;
+extern struct spinlock tcounterlock;
+extern uint     tcounter;
 
 
 // uart.c
